4d2c96590bb5eb8bc03edec22e262c1168dd9ef7
Bill Chen <billchen01@163.com>
2015-01-07 14:58:22 +0800

    init CwindJavaLab

---
+++ FileMonitor/src/main/java/com/cwind/App.java
@@ -0,0 +1,13 @@
+package com.cwind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
