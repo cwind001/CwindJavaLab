f5ff6806c4c99c19e64a7625dc43ba5bf84b908e
Bill Chen <billchen01@163.com>
2015-01-22 18:02:00 +0800

    Refine watchServiceTest

--- FileMonitor/src/test/java/com/cwind/file/FileMonitorTest.java
+++ FileMonitor/src/test/java/com/cwind/file/FileMonitorTest.java
@@ -1,7 +1,6 @@
 package com.cwind.file;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
--- FileMonitor/src/test/java/com/cwind/file/WatchServerTest.java
+++ FileMonitor/src/test/java/com/cwind/file/WatchServerTest.java
@@ -1,23 +1,20 @@
 package com.cwind.file;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardWatchEventKinds;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 
 public class WatchServerTest {
-	public static void main(String[] args) throws IOException, InterruptedException {
-		File sampleFile1 = new File("sample1.txt");
-		String absolutePath = sampleFile1.getAbsolutePath();
-		absolutePath = absolutePath.substring(0, absolutePath.lastIndexOf("\\"));
+	public static void main(String[] args) throws InterruptedException, IOException {
 		WatchService watchService = FileSystems.getDefault().newWatchService();
-		final Path path = FileSystems.getDefault().getPath(absolutePath);
-		System.out.println(path);
-		final WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+		final Path path = Paths.get(".");
+		final WatchKey watchKey = path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, 
+				StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE);
 		boolean fileNotChanged = true;
 		int count = 0;
 		while (fileNotChanged) {
@@ -26,8 +23,8 @@ public class WatchServerTest {
 		    for (WatchEvent<?> event : wk.pollEvents()) {
 		        //we only register "ENTRY_MODIFY" so the context is always a Path.
 		        final Path changed = (Path) event.context();
-		        System.out.println(changed);
-		        if (changed.endsWith(sampleFile1.getName())) {
+		        System.out.println(changed + ", " + event.kind());
+		        if (changed.endsWith("sample1.txt")) {
 		            System.out.println("Sample file has changed");
 		        }
 		    }
